c2122a12c8f43bdf6da0f26ff0ffbe12d90f4627
Kevin Friend <kfriend@rakacreative.com>
2018-10-15 12:41:29 -0400

    Import hello world

---
+++ Hello World/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hello World
+//
+//  Created by Kevin Friend on 10/15/18.
+//  Copyright © 2018 Kevin Friend. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
